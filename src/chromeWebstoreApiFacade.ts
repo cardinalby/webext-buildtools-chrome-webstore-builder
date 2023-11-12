17dcc791fae4f8604e440f40491a5b44b756136c
Cardinal <cardinalby@gmail.com>
2023-11-12 18:20:16 +0100

    add axios maxBodyLength, maxContentLength config values.

--- src/chromeWebstoreApiFacade.ts
+++ src/chromeWebstoreApiFacade.ts
@@ -27,7 +27,10 @@ export class ChromeWebstoreApiFacade {
     protected readonly _logWrapper: LoggerWrapper;
 
     public constructor(token: string, extensionId: string) {
-        this._api = new webstoreApi.WebstoreApi(token);
+        this._api = new webstoreApi.WebstoreApi(token, {
+            maxBodyLength: 2147483648, // 2gb
+            maxContentLength: 2147483648, // 2gb
+        });
         this._extensionId = extensionId;
         this._logWrapper = new LoggerWrapper();
     }
